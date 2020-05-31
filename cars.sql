6438ede6fa474d5e1320184e4bda63635db9f712
Angel Petkov <60899073+soilwork92@users.noreply.github.com>
2020-05-31 16:44:26 +0300

    Added test user in sql file

--- cars.sql
+++ cars.sql
@@ -1,8 +1,57 @@
-DROP TABLE IF EXISTS `admins`;
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 31, 2020 at 03:42 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
 
-DROP TABLE IF EXISTS `cars`;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cars`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admins`
+--
+
+CREATE TABLE `admins` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `hashed_password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `admins`
+--
+
+INSERT INTO `admins` (`id`, `first_name`, `last_name`, `email`, `username`, `hashed_password`) VALUES
+(3, 'Angel', 'Petkov', 'soilwork@mail.bg', 'soilwork', '$2y$10$Ln0pZAlvd2sLsGHbE8rcEe0JT.r9XFGNrdtvywFakzt02dfxjwZNm'),
+(4, 'Test', 'User', 'test@test.coom', 'testuser', '$2y$10$pWRQn0srjpBNM7mVbMKRK.YngbsayOb/3b9LKEA11j2AYAJXDQahC');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars`
+--
+
 CREATE TABLE `cars` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `brand` varchar(255) NOT NULL,
   `model` varchar(255) NOT NULL,
   `year` int(4) NOT NULL,
@@ -10,6 +59,56 @@ CREATE TABLE `cars` (
   `color` varchar(255) NOT NULL,
   `price` decimal(9,2) NOT NULL,
   `condition_id` tinyint(3) NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cars`
+--
+
+INSERT INTO `cars` (`id`, `brand`, `model`, `year`, `category`, `color`, `price`, `condition_id`, `description`) VALUES
+(1, 'Opel', 'Astra', 2017, 'Hybrid', 'black', '19000.00', 5, ''),
+(2, 'Honda', 'Civic', 2016, 'Electric', 'dark red', '15000.00', 5, ''),
+(3, 'Ford', 'Escort', 2016, 'City', 'orange', '14000.00', 4, ''),
+(4, 'Hyundai', 'Tuscon', 2015, 'Electric', 'blue', '27000.00', 2, ''),
+(5, 'Fiat', 'Spider', 2016, 'City', 'dark green', '17000.00', 3, ''),
+(6, 'Pegeout', '208', 2015, 'Hybrid', 'light gray', '21000.00', 3, ''),
+(13, 'Seat', 'Arona', 2007, 'City', 'white', '6000.00', 1, 'Good car for daily usage in the city.');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admins`
+--
+ALTER TABLE `admins`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `index_username` (`username`);
+
+--
+-- Indexes for table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admins`
+--
+ALTER TABLE `admins`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
